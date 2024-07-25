2c7785521748f33cd48d9a3bde89dd0b2504dbf4
Sean Kim <106803067+SeanKim912@users.noreply.github.com>
2024-07-25 00:29:08 -0500

    Time: 4742 ms (5.11%), Space: 17 MB (29.13%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,13 +1,16 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         solution = []
+        int_box = []
+        for x in boxes:
+            int_box.append(int(x))
         i = 0
-        while i < len(boxes):
+        while i < len(int_box):
             j = 0
             total = 0
-            while j < len(boxes):
+            while j < len(int_box):
                 diff = abs(i - j)
-                total += (diff * int(boxes[j]))
+                total += (diff * int_box[j])
                 j += 1
             solution.append(total)
             i += 1
