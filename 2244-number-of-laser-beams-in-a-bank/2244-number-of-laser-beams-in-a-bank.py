fbb56cbe0ecd5dba60a87ceba50e09d7821733c4
Sean Kim <106803067+SeanKim912@users.noreply.github.com>
2024-07-25 01:56:45 -0500

    Time: 89 ms (70.64%), Space: 18.4 MB (99.58%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.py
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.py
@@ -3,19 +3,23 @@ class Solution:
         total = 0
         i = 0
         length = len(bank)
+        while i < length:
+            string = bank[i]
+            bank[i] = string.count("1")
+            i += 1
+        
+        i = 0
         while i < length - 1:
             r1 = bank[i]
-            if "1" in r1:    
-                r2 = ""
-                j = i + 1
-                while j < length:
-                    if "1" in bank[j]:
-                        r2 = bank[j]
-                        j = length
-                    else:
-                        j += 1
-                
-                total += (r1.count("1") * r2.count("1"))
+            r2 = 0
+            j = i + 1
+            while j < length:
+                if bank[j]:
+                    r2 = bank[j]
+                    j = length
+                else:
+                    j += 1
+            total += (r1 * r2)
             i += 1
 
         return total
