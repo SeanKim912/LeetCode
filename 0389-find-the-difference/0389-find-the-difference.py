6ba3db57951e810d0f2cb902b9d86bfab2b2eb26
Sean Kim <106803067+SeanKim912@users.noreply.github.com>
2025-05-27 02:48:43 -0500

    Time: 5 ms (14.73%), Space: 17.8 MB (42.45%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -2,14 +2,32 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         s_list = [char for char in s]
         t_list = [char for char in t]
-        s_list.sort()
-        t_list.sort()
-        i = 0
 
-        while i < len(t) - 1:
-            if t_list[i] != s_list[i]:
-                return t_list[i]
+        s_dict = {}
+        t_dict = {}
+
+        i = 0
+        while i < len(s_list):
+            curr = s_list[i]
+            if curr not in s_dict:
+                s_dict[curr] = 1
             else:
-                i += 1
-        
-        return t_list[-1]
+                s_dict[curr] += 1
+
+            i += 1
+
+        j = 0
+        while j < len(t_list):
+            curr = t_list[j]
+            if curr not in t_dict:
+                t_dict[curr] = 1
+            else:
+                t_dict[curr] += 1
+
+            j += 1
+
+        for letter in t_dict:
+            if letter not in s_dict:
+                return letter
+            elif t_dict[letter] != s_dict[letter]:
+                return letter
